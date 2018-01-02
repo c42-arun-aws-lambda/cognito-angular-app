0707d419fe50887098a0e33cb7fce5532e886f22
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-01-02 13:18:37 +0000

    signup & confirm registration added

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -5,10 +5,20 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+
 import { User } from './user.model';
+
+const POOL_DATA = {
+  UserPoolId: 'us-east-2_TnbeeeyO1',
+  ClientId: '6l6ok671fpqsj25jlubrdsdr3d'
+};
+
+const userPool = new CognitoUserPool(POOL_DATA);
 
 @Injectable()
 export class AuthService {
+  registeredUser: CognitoUser;
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
@@ -24,13 +34,41 @@ export class AuthService {
       Name: 'email',
       Value: user.email
     };
+
+    const attrList: CognitoUserAttribute[] = [];
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+
+    userPool.signUp(username, password, attrList, null, (err, result) => {
+      if (err) {
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      this.registeredUser = result.user;
+    });
+
     return;
   }
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
     const userData = {
       Username: username,
+      Pool: userPool
     };
+
+    const cognitoUser = new CognitoUser(userData);
+    cognitoUser.confirmRegistration(code, true, (err, result) => {
+      if (err) {
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      this.router.navigate(['/']);
+    });
   }
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
